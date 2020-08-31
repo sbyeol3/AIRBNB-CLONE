543ac6df6127fb5beef7f4ecb8b7700a31a9ce81
sbyeol3 <ahnsbyeol3@gmail.com>
2020-08-31 22:23:51 +0900

    Feat : set Static Files using path module and link CSS file (#1)

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 const route = require('./router.js')
 
 app.set('view engine', 'pug')
+app.use('/css', express.static('./views/css'));
+app.use('/static', express.static('./views/static'));
 
 app.use('/', route);
 app.listen(port, () => {
