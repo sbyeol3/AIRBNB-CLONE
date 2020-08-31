eff97637adeae27e156a2d11369013a5e33f4943
sbyeol3 <ahnsbyeol3@gmail.com>
2020-08-31 21:21:39 +0900

    Feat : set View engine and create example pug file (#1)

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const route = require('./router.js')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.set('view engine', 'pug')
 
+app.use('/', route);
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
