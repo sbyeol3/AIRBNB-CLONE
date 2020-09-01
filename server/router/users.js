2df522649d924b6f6d0145c6c4c5b15ef9a3eb04
sbyeol3 <ahnsbyeol3@gmail.com>
2020-09-01 15:19:33 +0900

    Feat : create pug file (#8)

--- server/router/users.js
+++ server/router/users.js
@@ -2,8 +2,18 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/register', (req, res) => {
-    console.log(req)
+    // console.log(req)
     res.render('register')
+})
+
+router.get('/login', (req, res) => {
+    // console.log(req)
+    res.render('login')
+})
+
+router.post('/login', (req, res) => {
+    console.log(req)
+    // res.render('login')
 })
 
 module.exports = router
