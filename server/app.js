95d5362cce301901cf1c0eb887b78816074910ce
sbyeol3 <ahnsbyeol3@gmail.com>
2020-09-01 13:45:05 +0900

    Feat : signup page routed /users/register (#5)

--- server/app.js
+++ server/app.js
@@ -2,13 +2,15 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = 3000
-const route = require('./router.js')
+const mainRouter = require('./router/index.js')
+const userRouter = require('./router/users.js')
 
 app.set('view engine', 'pug')
-app.use('/css', express.static('./views/css'));
-app.use('/static', express.static('./views/static'));
+app.use('/css', express.static('./views/css'))
+app.use('/static', express.static('./views/static'))
 
-app.use('/', route);
+app.use('/', mainRouter)
+app.use('/users', userRouter)
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Server running at http://localhost:${port}...`)
 })
--- server/router/users.js
+++ server/router/users.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.render('index')
+router.get('/register', (req, res) => {
+    console.log(req)
+    res.render('register')
 })
 
 module.exports = router
