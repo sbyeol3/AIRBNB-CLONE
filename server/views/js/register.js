48cdbdf243becece5c23944ac378632784462064
sbyeol3 <ahnsbyeol3@gmail.com>
2020-09-02 17:34:54 +0900

    Feat : create funtion and Object about birthday section of Register form (#4)
    - 회원가입 폼 select tag에 효율적으로 option 태그들을 넣기 위해 Array, Object를 활용함
    - 30일 31일을 변경하는 방법은 추후에 추가하는 것으로 함
    - form tag 완료

--- server/views/js/register.js
+++ server/views/js/register.js
@@ -1,8 +1,7 @@
 const emailRegExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i
 const registerForm = document.getElementById("register-form")
 const inputs = document.getElementsByTagName("input")
-
-console.log(inputs)
+const { month, day, year } = document.getElementsByTagName("select")
 
 const condition = {
     email: {regexp: emailRegExp},
@@ -10,6 +9,12 @@ const condition = {
     lastName: {length: 1},
     password: {length: 6}
 }
+
+const birthday = [
+    {tag: month, text: '월', add: '월', start: 1, end: 12},
+    {tag: day, text: '일', start: 1, end: 31, minus: [2,4,6,9,11]},
+    {tag: year, text: '년', start: 2020, end: 1900, reverse: true},
+]
 
 const checkCondition = (tag, name, value) => {
     const {regexp, length} = condition[name]
@@ -25,4 +30,20 @@ for (let tag of inputs) {
         console.log(tag, name, value)
         checkCondition(tag, name, value)
     })
+}
+
+for (let item of birthday) {
+    const {tag, text, add='', start, end, reverse = false} = item
+    if (reverse) {
+        for (let i = start+1; i > end - 1; i--) {
+            const inner = (i === start+1) ? `<option disabled selected>${text}</option>` : `<option value={i}>${i}${add}</option>`
+            tag.innerHTML += inner
+        }
+    } else {
+        for (let i = start-1; i<= end; i++) {
+            const inner = (i === start-1) ? `<option disabled selected>${text}</option>` : `<option value={i}>${i}${add}</option>`
+            tag.innerHTML += inner
+        }
+    }
+
 }
