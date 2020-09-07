22288b888f4b4903cd1ede69883a34e3ee6f6775
sbyeol3 <ahnsbyeol3@gmail.com>
2020-09-07 22:26:22 +0900

    Update : change calendar layout
    - cell size 조절이 어려워 기존 table 구조에서 flex 구조로 변경
    - 과거시점인 경우 색상 변경
    - 체크인, 체크아웃 클릭 시 캘린더 on/off

--- server/views/js/calendar.js
+++ server/views/js/calendar.js
@@ -1,9 +1,15 @@
+const calendars = document.getElementsByClassName('calendar')
+
+for (let calendar of calendars) {
+    const calendarLayer = document.getElementById("calendar")
+    calendar.addEventListener("click", () => {
+        if (calendarLayer.style.visibility === "hidden") calendarLayer.style.visibility = "visible"
+        else calendarLayer.style.visibility = "hidden"
+    })
+}
+
 const thirty = [4,6,9,11]
 const picker = document.getElementsByClassName('month')
-
-const getFirstDay = (year=2020, month) => {
-    return new Date(year, month-1, 1).getDay()
-}
 
 for (let m of picker) {
     const month = m.getAttribute("month")
@@ -11,16 +17,22 @@ for (let m of picker) {
     if (month === 2) days = 28
     else if (thirty.includes(+month)) days = 30
 
-    const firstDay = getFirstDay(2020, month)
-    console.log(firstDay)
+    const firstDay = new Date(2020, month-1, 1).getDay()
+    const today = new Date().getDate()
+    const thisMonth = new Date().getMonth() + 1
+
     let day = 0
     for (let i=0; i<6; i++) {
-        const row = m.insertRow(m.rows.length)
+        let innerOfRow = ''
         for (let j=0; j<7; j++) {
-            if (i === 0 && day === 0 && firstDay !== j) row.insertCell().innerHTML = '<div class="disabled"/>'
-            else if (day === days) break
-            else row.insertCell().innerHTML = `<div class="day">${day++ + 1}</div>`
+            if (day === days) break
+            else {
+                if (i === 0 && day === 0 && firstDay !== j) innerOfRow += '<span class="day disable"></span>'
+                else if (day < today - 1 && +month === thisMonth) innerOfRow += `<span class="day disable past">${day++ + 1}</span>`
+                else innerOfRow += `<span class="day able">${day++ + 1}</span>`
+            }
         }
-        
+        const row = `<div class="week">${innerOfRow}</div>`
+        m.innerHTML += row
     }
 }
