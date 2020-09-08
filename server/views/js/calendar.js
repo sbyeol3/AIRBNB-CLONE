a50c0327ba321c7d188043715dc31de5eec31895
sbyeol3 <ahnsbyeol3@gmail.com>
2020-09-08 16:48:15 +0900

    Feat : add calendar click event to visualize (#9)
    - 체크인, 체크아웃이 선택되었음을 사용자에게 보여주기 위해 CSS 효과 추가
    - 체크인, 체크아웃 값은 Object 이용, 조건 추가 (체크인 < 체크아웃)
    - 체크인 ~ 체크아웃 사이 날짜들은 배경 색칠하게끔
    - 해결해야 하는 문제 : 단일태그라서 bg color와 hover 효과가 겹치는 문제 있음

--- server/views/js/calendar.js
+++ server/views/js/calendar.js
@@ -29,10 +29,70 @@ for (let m of picker) {
             else {
                 if (i === 0 && day === 0 && firstDay !== j) innerOfRow += '<span class="day disable"></span>'
                 else if (day < today - 1 && +month === thisMonth) innerOfRow += `<span class="day disable past">${day++ + 1}</span>`
-                else innerOfRow += `<span class="day able">${day++ + 1}</span>`
+                else innerOfRow += `<span class="day able" month=${month} day=${day+1}>${day++ + 1}</span>`
             }
         }
         const row = `<div class="week">${innerOfRow}</div>`
         m.innerHTML += row
     }
+}
+
+const ableElements = document.getElementsByClassName('able')
+const selectedDate = {
+    checkin: null,
+    checkout: null
+}
+
+const getDateFormat = (el) => {
+    const day = el.getAttribute('day')
+    const month = el.getAttribute('month')
+    return `2020-${+month < 10 ? '0' : ''}${month}-${+day < 10 ? '0' : ''}${day}`
+}
+
+const resetClassSelected = () => {
+    const checkinElement = document.getElementsByClassName('sel-checkin')[0]
+    checkinElement.classList.remove('sel-checkin')
+
+    const checkoutElement = document.getElementsByClassName('sel-checkout')[0]
+    if (checkoutElement) {
+        checkoutElement.classList.remove('sel-checkout')
+    }
+}
+
+const addBgColor = () => {
+    const { checkin, checkout } = selectedDate
+    const dayElements = document.getElementsByClassName('day')
+    for (let el of dayElements) {
+        const date = getDateFormat(el)
+        if (checkin < date && date < checkout) {
+            el.classList.add('sel-between')
+        }
+    }
+}
+
+const changeSelectedDate= (el) => {
+    const date = getDateFormat(el)
+
+    if (!selectedDate.checkin) {
+        selectedDate.checkin = date
+        el.classList.add('sel-checkin')
+    } else if (!selectedDate.checkout && selectedDate.checkin < date) {
+        selectedDate.checkout = date
+        el.classList.add('sel-checkout')
+        addBgColor()
+    } else if (selectedDate.checkin === date) {
+        selectedDate.checkin = null
+        selectedDate.checkout = null
+        resetClassSelected()
+    } else {
+        selectedDate.checkin = date
+        selectedDate.checkout = null
+        resetClassSelected()
+        el.classList.add('sel-checkin')
+    }
+    console.log(selectedDate)
+}
+
+for (let el of ableElements) {
+    el.addEventListener('click', () => changeSelectedDate(el))
 }
