05cf60efa1e8b3b1559ca6cb052019388353e439
sbyeol3 <ahnsbyeol3@gmail.com>
2020-09-08 23:21:18 +0900

    Feat : implement functions to control guest counts (#9)
    - 사용자에게 더 정확하게 알려주기 위해 인원이 현재 0일 때는 - 버튼 비활성화
    - 플러스 마이너스 버튼을 누를 때마다 현재 인원 값이 0인지 아닌지 체크하기
    - disabled 상태일 때 css 효과 추가
    - 현재 인원 값은 각 게스트 종류에 있는 div.num에 attribute로 넣어둠

--- server/views/js/guest.js
+++ server/views/js/guest.js
@@ -9,3 +9,44 @@ const controlGuestLayer = () => {
     } else guestLayer.style.visibility = "hidden"
 }
 guestInput.addEventListener('click', () => controlGuestLayer())
+
+const guestCountElements = document.getElementsByClassName('guest-count')
+
+const handleMinusButton = (childNodes) => {
+    const minusButton = childNodes[0]
+    const numElement = childNodes[1]
+    if (+numElement.getAttribute('value') === 0) {
+        minusButton.setAttribute('disabled', true)
+    } else if (minusButton.getAttribute('disabled')) {
+        minusButton.removeAttribute('disabled')
+    }
+}
+
+const onClickMinus = (numElement) => {
+    const currentValue = +numElement.getAttribute('value')
+    numElement.setAttribute('value', currentValue - 1)
+    numElement.innerHTML = currentValue - 1
+}
+
+const onClickPlus = (numElement) => {
+    const currentValue = +numElement.getAttribute('value')
+    numElement.setAttribute('value', currentValue + 1)
+    numElement.innerHTML = currentValue + 1
+}
+
+for (let countElement of guestCountElements) {
+    const { childNodes } = countElement
+    const minusButton = childNodes[0]
+    const numElement = childNodes[1]
+    const plusButton = childNodes[2]
+
+    handleMinusButton(childNodes)
+    minusButton.addEventListener('click', () => {
+        onClickMinus(numElement)
+        handleMinusButton(childNodes)
+    })
+    plusButton.addEventListener('click', () => {
+        onClickPlus(numElement)
+        handleMinusButton(childNodes)
+    })
+}
